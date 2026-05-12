91c6455a4497dd65ad9e3e20e7a8efcb322a42fb
Sakshi-1224 <sakshi12bpl@gmail.com>
2026-05-12 11:23:50 +0530

    apihost for emulator

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -3,10 +3,10 @@ class AppConfig {
   // after running BOTH commands: 
   // adb reverse tcp:3000 tcp:3000
   // adb reverse tcp:9000 tcp:9000
-  static const String apiHost = '127.0.0.1'; 
+  //static const String apiHost = '127.0.0.1';
   
   // Use '10.0.2.2' ONLY when testing on the Android Emulator
-  // static const String apiHost = '10.0.2.2';
+   static const String apiHost = '10.0.2.2';
 
   static const String apiPort = '3000';
   static const String apiBaseUrl = 'http://$apiHost:$apiPort/api/v1';
